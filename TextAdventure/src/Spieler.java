a771ea739c3abc0c9754b152288a1c6357e3b0f0
knautschka <knautschka@googlemail.com>
2022-07-17 22:12:41 +0200

    Koordinatensytem hinzugefügt

--- TextAdventure/src/Spieler.java
+++ TextAdventure/src/Spieler.java
@@ -5,15 +5,21 @@ public class Spieler {
 	private int leben;
 	private int angriff;
 	private int verteidigung;
+	private int positionLinks;
+	private int positionRechts;
+	private int positionGeradeaus;
+	private int positionRueckwaerts;
 	
 	
-	
-	public Spieler(String spielerName, int leben, int angriff, int verteidigung) {
-		super();
+	public Spieler(String spielerName, int leben, int angriff, int verteidigung, int positionLinks, int positionRechts, int positionGeradeaus, int positionRueckwaerts) {
 		this.spielerName = spielerName;
 		this.leben = leben;
 		this.angriff = angriff;
 		this.verteidigung = verteidigung;
+		this.positionLinks = positionLinks;
+		this.positionRechts = positionRechts;
+		this.positionGeradeaus = positionGeradeaus;
+		this.positionRueckwaerts = positionRueckwaerts;
 	}
 	
 	
@@ -40,6 +46,47 @@ public class Spieler {
 	}
 	public void setVerteidigung(int verteidigung) {
 		this.verteidigung = verteidigung;
+	}
+
+	
+	
+	public int getPositionLinks() {
+		return positionLinks;
+	}
+
+
+	public void setPositionLinks(int positionLinks) {
+		this.positionLinks = positionLinks;
+	}
+
+
+	public int getPositionRechts() {
+		return positionRechts;
+	}
+
+
+	public void setPositionRechts(int positionRechts) {
+		this.positionRechts = positionRechts;
+	}
+
+
+	public int getPositionGeradeaus() {
+		return positionGeradeaus;
+	}
+
+
+	public void setPositionGeradeaus(int positionGeradeaus) {
+		this.positionGeradeaus = positionGeradeaus;
+	}
+
+
+	public int getPositionRueckwaerts() {
+		return positionRueckwaerts;
+	}
+
+
+	public void setPositionRueckwaerts(int positionRueckwaerts) {
+		this.positionRueckwaerts = positionRueckwaerts;
 	}
 
 
